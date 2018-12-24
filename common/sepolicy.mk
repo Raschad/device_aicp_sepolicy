69698d73a53cda3214d4d3604b8b70851000f414
Raschad <Raschad@yandex.ru>
2018-12-24 20:07:49 +0300

    Update sepolicy.mk

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -4,10 +4,10 @@
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/pixys/sepolicy/common/public
+    device/arrow/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/pixys/sepolicy/common/private
+    device/arrow/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/pixys/sepolicy/common/vendor
+    device/arrow/sepolicy/common/vendor
